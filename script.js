906a8809b6bfa44db38dedc093e3f0b4ba36c682
devkushal1996 <dev.kushal1996@gmail.com>
2025-04-03 13:49:24 -0400

    Winning Conditions and Winner Tested

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelectorAll("#reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
 
 let turn0 = true;
 
@@ -25,5 +29,27 @@ boxes.forEach( (box) => {
             turn0 = true;
         }
        box.disabled = true;
+
+       checkWinner();
     });
 });
+
+const showWinner = (winner) => {
+    msg.innerText = `Congrats!! The Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log ("winner",pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
